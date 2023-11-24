38e7e6f6423ce4bc9b8d318dd37d2af62ae02695
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2023-11-24 22:15:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if num_1 in data and num_2 in data:
+        return num_1 + num_2
+    else: 
+        return jsonify({'error': 'Please provide num1 and num2 in JSON format.'}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    if num_1 in data and num_2 in data:
+        return num_1 - num_2
+    else: 
+        return jsonify({'error': 'Please provide num1 and num2 in JSON format.'}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
